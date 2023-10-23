ddbeb33f3981b47850989faa28000dc05b042797
Jason Inouye <jasonjinouye@gmail.com>
2023-10-23 11:00:44 -0500

    refactor blog form component

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -33,5 +33,5 @@ const deleteBlogPost = dispatch => {
 export const { Context, Provider } = createDataContext(
     blogReducer,
     { addBlogPost, deleteBlogPost },
-    []
+    [{ title: 'TEST POST', content: 'This is test content for my blog post', id: 1 }]
 );
--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import { Context } from '../context/BlogContext';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { EvilIcons } from '@expo/vector-icons';
 
 const ShowScreen = ({ navigation }) => {
     const {state} = useContext(Context);
@@ -11,9 +13,22 @@ const ShowScreen = ({ navigation }) => {
             <Text>
                 {blogPost.title}
             </Text>
+            <Text>
+                {blogPost.content}
+            </Text>
         </View>
     )
 };
+
+ShowScreen.navigationOptions = ({ navigation }) => {
+    return {
+        headerRight: () => (
+            <TouchableOpacity onPress={() => navigation.navigate('Edit', { id: navigation.getParam('id')})}>
+                <EvilIcons name="pencil" size={35} />
+            </TouchableOpacity>
+        ),
+    }
+}
 
 const styles = StyleSheet.create({});
 
